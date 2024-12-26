4f4711e96a8573ed5e60b51945065bc3c4a6123f
giorgizena123 <zenaishvili621@gmail.com>
2024-12-27 00:52:25 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Music music1 = new Music("Song1", 180, "Pop");
+        Music music2 = new Music("Song2", 240, "Rock");
+        Music music3 = new Music("Song3", 300, "Jazz");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Album album1 = new Album("Best Hits", 2023);
+        album1.addMusic(music1);
+        album1.addMusic(music2);
+
+        Album album2 = new Album("New Sounds", 2024);
+        album2.addMusic(music3);
+
+        Artist artist1 = new Artist("luka");
+        artist1.addAlbum(album1);
+        artist1.addAlbum(album2);
+
+        MusicLibrary library = new MusicLibrary();
+        library.addArtist(artist1);
+
+
+        Music foundMusic = library.searchMusic("Song1");
+        if (foundMusic != null) {
+            System.out.println("Found Music: " + foundMusic.getTitle());
+        } else {
+            System.out.println("Music not found.");
         }
+
+
+        Music randomMusic = library.giveRandomMusic();
+        if (randomMusic != null) {
+            System.out.println("Random Music: " + randomMusic.getTitle());
+            randomMusic.play();
+            randomMusic.stop();
+        }
+
+        artist1.displayInfo();
+        album1.displayInfo();
     }
 }
