83a23d8615c7dd8eaca73ed3d9c3f1a60d79d9d7
giorgizena123 <zenaishvili621@gmail.com>
2024-12-27 00:52:11 +0400

    Update Artist.java

--- src/Artist.java
+++ src/Artist.java
@@ -1,4 +1,74 @@
-package PACKAGE_NAME;
 
 public class Artist {
+    private String name;
+    private Album firstAlbum;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addAlbum(Album album) {
+
+        if (firstAlbum == null) {
+            firstAlbum = album;
+            return;
+        }
+
+
+        Album currentAlbum = firstAlbum;
+        while (currentAlbum.getNext() != null) {
+            currentAlbum = currentAlbum.getNext();
+        }
+
+
+        currentAlbum.setNext(album);
+    }
+
+    public void removeAlbum(Album album) {
+        if (firstAlbum == null) {
+            return;
+        }
+
+
+        if (firstAlbum == album) {
+            firstAlbum = firstAlbum.getNext();
+            return;
+        }
+
+
+        Album currentAlbum = firstAlbum;
+        while (currentAlbum.getNext() != null && currentAlbum.getNext() != album) {
+            currentAlbum = currentAlbum.getNext();
+        }
+
+
+        if (currentAlbum.getNext() != null) {
+
+            currentAlbum.setNext(currentAlbum.getNext().getNext());
+        }
+    }
+
+    public void displayInfo() {
+        System.out.println("Artist: " + name);
+        Album currentAlbum = firstAlbum;
+        while (currentAlbum != null) {
+            currentAlbum.displayInfo();
+            currentAlbum = currentAlbum.getNext();
+        }
+    }
+
+    public Artist getNext() {
+        return null;
+    }
+
+    public void setNext(Artist artist) {
+    }
 }
